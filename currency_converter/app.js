4b11610e401e4a12bce893bde8ef505685544849
Rajinikanth <kundelurajini7@gmail.com>
2024-10-28 09:37:55 +0530

    currency_converter

---
+++ currency_converter/app.js
@@ -0,0 +1,28 @@
+let formSelect = document.querySelectorAll(".toFrom select")
+
+for(let select of formSelect){
+    for(currCode in countryList){
+        let newOption = document.createElement("option")
+        newOption.innerText = currCode
+        newOption.value = currCode
+
+        if(select.name === "from" && currCode === "USD")
+        {
+            newOption.selected = "selected"
+        } else if(select.name === "to" && currCode === "INR")
+        {
+                newOption.selected = "selected"
+        }
+        select.append(newOption)
+    }
+    select.addEventListener("change", (evt) => {updateFlag(evt.target)})
+}
+
+const updateFlag = (element) => {
+    let currCode = element.value
+    let countryCode  = countryList[currCode]
+    let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`
+    let img = element.parentElement.querySelector("img")
+    img.src = newSrc;
+
+}
